98179bce86e29108b56b8b061ff9c65cb37551e0
Cryxnoob <janos.saelzer@th-bingen.de>
2017-10-28 12:57:44 +0200

    Segment class für das vorgegebene main
    Hier wird gezählt, wie hoch die Auslastung eines Segments ist

---
+++ Segment.kt
@@ -0,0 +1,25 @@
+package Project2
+
+class Segment(id:Int) {
+    private var currentcount:Int = 0
+    private var id:Int = id
+
+    public fun getid():Int{
+        return id
+    }
+
+
+    public fun setcurrentcount(c: Int) {
+
+        currentcount = c
+    }
+
+    public fun getcurrentcount():Int{
+        return currentcount
+    }
+
+    fun increasecount(){
+
+        currentcount++
+    }
+}
