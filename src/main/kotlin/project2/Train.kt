cc91dab50e8740ca4c6b0376529d4cc1724ecb8f
Marcel Espenschied <espenschied.marcel@web.de>
2017-10-28 20:39:24 +0200

    Habe die TrainClass hinzugefügt

--- src/main/kotlin/project2/Train.kt
+++ src/main/kotlin/project2/Train.kt
@@ -1,4 +1,24 @@
 package project2
 
-class Train {
+package project2
+
+import java.util.*
+
+class Train(schedule:MutableList<Int>) {
+
+    private var delayed:Boolean = false
+    private var schedule:List<Int> = schedule
+
+    public fun setDelayed(d: Boolean){
+        delayed= d
+    }
+
+    public fun getDelayed():Boolean{
+        return delayed
+    }
+    public fun getcurrentSegment():Int{
+
+
+        return schedule[0]
+    }
 }
