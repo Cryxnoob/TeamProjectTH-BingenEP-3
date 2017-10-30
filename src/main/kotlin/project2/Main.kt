6da6350b34393eaf353434c72e9ede9e931a0a60
Janos Sälzer <janos.saelzer@th-bingen.de>
2017-10-30 13:33:04 +0100

    Kleine Verbesserungen

--- src/main/kotlin/project2/Main.kt
+++ src/main/kotlin/project2/Main.kt
@@ -8,21 +8,40 @@ import com.univocity.parsers.csv.CsvWriterSettings
 
 fun main(args:Array<String>) {
 
-    loadCSV()
+    scneario()
+    external()
 
 }
 
+fun scneario (){
+    var train1 = Train(schedule=mutableListOf(5,2,3,0,1))
+    var train2 = Train(schedule=mutableListOf(4,0,0,3,4))
+    var train3 = Train(schedule=mutableListOf(1,2,3,4,5))
+    var train4 = Train(schedule=mutableListOf(5,4,3,2,1))
+    var train5 = Train(schedule=mutableListOf(0,0,5,5,2))
+    var train6 = Train(schedule=mutableListOf(3,4,5,1,2))
+    var train7 = Train(schedule=mutableListOf(2,1,1,1,3))
+    var train8 = Train(schedule=mutableListOf(1,1,0,0,0))
+    var train9 = Train(schedule=mutableListOf(4,3,2,5,5))
+    var train10 = Train(schedule=mutableListOf(1,5,4,4,4))
+    var train11 = Train(schedule=mutableListOf(2,0,4,3,3))
+    var train12 = Train(schedule=mutableListOf(3,0,0,0,0))
+    var trains = mutableListOf(train1, train2, train3, train4, train5, train6, train7, train8, train9, train10, train11, train12)
 
-fun loadCSV(){
-    var trains:MutableList<Train> = parseCSV()
-    runScenario(trains)
-}
-
-fun runScenario(trains:MutableList<Train>){
-    var network = Network(3, trains, 5)
+    var network = Network(3, trains, 5, 5)
     network.createSegments()
     network.simulateoneStep()
-    safeResultsToCSV(trains)
+    writeresultstoconsole(trains)
+}
+
+
+fun external (){
+    var trains:MutableList<Train> = parseCSV()
+    var network = Network(3, trains, 5,trains[0].getschedulelenght())
+    network.createSegments()
+    network.simulateoneStep()
+    saveResultsToCSV(trains)
+
 }
 
 fun parseCSV():MutableList<Train> {
@@ -54,7 +73,7 @@ fun parseCSV():MutableList<Train> {
     }
     return trainsCSV
 }
-fun safeResultsToCSV(results: List<Train>, outputFile: String = "results.csv") {
+fun saveResultsToCSV(results: List<Train>, outputFile: String = "results.csv") {
     val writer = FileHandler().getWriter(outputFile)
 
     val csvWriter = CsvWriter(writer, CsvWriterSettings())
@@ -71,13 +90,31 @@ fun safeResultsToCSV(results: List<Train>, outputFile: String = "results.csv") {
         i++
 
         if (result.getDelayed()){
-            row = arrayOf(i.toString(), result.getcurrentSegment().toString(), "delayed")
+            row = arrayOf(i.toString(), result.getcurrentSegment(0).toString(), "delayed")
         }
         else {
-            row = arrayOf(i.toString(), result.getcurrentSegment().toString(), "ontime")
+            row = arrayOf(i.toString(), result.getcurrentSegment(0).toString(), "ontime")
         }
         trainrows.add (row)
     }
 
     csvWriter.writeRowsAndClose(trainrows)
+}
+
+fun writeresultstoconsole(results: List<Train>) {
+
+    var i:Int = 0
+    for (result in results){
+
+        i++
+
+        if (result.getDelayed()){
+            println (i.toString()+ " - delayed")
+        }
+        else {
+            println (i.toString()+" - ontime")
+        }
+
+    }
+
 }
