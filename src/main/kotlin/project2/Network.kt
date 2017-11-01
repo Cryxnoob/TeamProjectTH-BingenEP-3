389989a84445b315f96b3c7bf6d48c942e7a42b9
Benedict Galinski <bene-galinski@t-online.de>
2017-11-01 16:38:50 +0100

    Habe Netzwerk angepasst

--- src/main/kotlin/project2/Network.kt
+++ src/main/kotlin/project2/Network.kt
@@ -1,16 +1,28 @@
 package project2
 
-class Network(val capacity:Int,val trains:List<Train>, val segmentcount:Int,val schedulelength:Int) {
+import java.util.*
+
+class Network(val capacity:Int,val trains:List<Train>, val schedulelength:Int, val capacities: MutableList<Int>) {
     private var segments:MutableList<Segment> = mutableListOf()
     private var currentstep:Int = 0
-    init {
-        for (x in 0..segmentcount) {
-            segments.add(Segment(x+1))
+
+    init{
+
+        var x:Int= 1
+        for (capacity in capacities){
+            segments.add(Segment(x, capacity))
+            x++
         }
+
 
     }
 
     fun simulateoneStep(){
+
+        var diff:Int
+        var x:Int
+        var y:Int
+        var delayedtrains:MutableList<Int> = mutableListOf()
         /*
         reset segment Train count
         */
@@ -30,12 +42,25 @@ class Network(val capacity:Int,val trains:List<Train>, val segmentcount:Int,val 
         }
 
         for (segment in segments){
-            if (segment.getcurrentcount()>=capacity){
+            if (segment.getcurrentcount()>segment.capacity){
 
+                diff = segment.getcurrentcount()-segment.capacity
+                x = diff
+                while (x > 0 ){
+                    delayedtrains.add(Random().nextInt(segment.getcurrentcount()))
+                    x--
+                }
+                y = 0
 
                 for (train in trains){
-                    if (train.getcurrentSegment(currentstep)==segment.id) {
-                        train.setDelayed(true)
+                    if (train.getcurrentSegment(currentstep)==segment.id){
+                        for (delayedtrain in delayedtrains){
+                            if (delayedtrain == y){
+                                train.setDelayed(true)
+                            }
+                        }
+
+                        y++
                     }
 
                 }
