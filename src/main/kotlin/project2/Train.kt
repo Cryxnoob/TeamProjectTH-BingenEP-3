b7022aaa6a6c337baca3b68558f14a0e2e30cf59
Marcel Espenschied <espenschied.marcel@web.de>
2017-11-02 15:38:05 +0100

    Train wurde angepasst

--- src/main/kotlin/project2/Train.kt
+++ src/main/kotlin/project2/Train.kt
@@ -2,9 +2,11 @@ package project2
 
 import java.util.*
 
-class Train(val schedule:MutableList<Int>) {
+class Train(var schedule:MutableList<Int>, val priority: Int, val id: Int) {
 
     private var delayed:Boolean = false
+    private var scheduleDriven: MutableList<Int> = mutableListOf()
+    private var delayList: MutableList<Boolean> = mutableListOf()
 
     public fun setDelayed(d: Boolean){
         delayed= d
@@ -21,5 +23,32 @@ class Train(val schedule:MutableList<Int>) {
     fun getScheduleLength():Int{
 
         return schedule.size
+    }
+
+    public fun shiftSchedule(stepNumber: Int) {
+        var x: Int = 0
+        var tempSchedule: MutableList<Int> = mutableListOf()
+        for (slot in schedule) {
+            if (x == stepNumber) {
+                tempSchedule.add(-1)
+            }
+            else if (x > stepNumber) {
+                tempSchedule.add(schedule[x-1])
+            }
+            else if (x < stepNumber) {
+                tempSchedule.add(schedule[x])
+            }
+            x++
+        }
+        schedule = tempSchedule
+    }
+
+    public fun persistStep(stepNumber: Int) {
+        scheduleDriven.add(schedule[stepNumber])
+        delayList.add(delayed)
+    }
+
+    public fun getScheduleDriven(): MutableList<Int> {
+        return scheduleDriven
     }
 }
