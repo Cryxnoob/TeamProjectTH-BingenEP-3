cd60a1cd289b7cea79cf7c6d9b7d8065bcb5b853
Benedict Galinski <bene-galinski@t-online.de>
2017-11-02 15:25:16 +0100

    Netzwerk wurde auf Main und CSV angepasst

--- src/main/kotlin/project2/Network.kt
+++ src/main/kotlin/project2/Network.kt
@@ -5,6 +5,10 @@ import java.util.*
 class Network(val capacity:Int,val trains:List<Train>, val scheduleLength:Int, val capacities: MutableList<Int>) {
     private var segments:MutableList<Segment> = mutableListOf()
     private var currentStep:Int = 0
+    private var probPrio1: Int = 0
+    private var probPrio2: Int = 0
+    private var probPrio3: Int = 0
+    private var segmentTrainMapping: MutableList<MutableList<Train>> = mutableListOf()
 
     init{
 
@@ -17,65 +21,149 @@ class Network(val capacity:Int,val trains:List<Train>, val scheduleLength:Int, v
 
     }
 
+    fun runSimulation() {
+        while (currentStep < scheduleLength) {
+            simulateOneStep()
+        }
+    }
+
+
     fun simulateOneStep(){
 
-        var diff:Int
-        var x:Int
-        var y:Int
-        var delayedTrains:MutableList<Int> = mutableListOf()
-        /*
-        reset segment Train count
-        */
-        for (segment in segments) {
 
+        var diff: Int
+        var x: Int
+        var delayedTrains: MutableList<Int> = mutableListOf()
+        /*
+        reset segment train count
+        */
+        segmentTrainMapping = mutableListOf()
+        for(segment in segments) {
             segment.setCurrentCount(0)
+            segmentTrainMapping.add(mutableListOf())
         }
-        for (train in trains){
+        for(train in trains) {
             train.setDelayed(false)
         }
-        for (train in trains){
 
-            var segment:Int = train.getCurrentSegment(currentStep)-1
-            if (segment>-1){
+        for(train in trains) {
+            var segment:Int = train.getCurrentSegment(currentStep) - 1
+            if(segment > -1) {
                 segments[segment].increaseCount()
+                segmentTrainMapping[segment].add(train)
             }
         }
 
-        for (segment in segments){
-            if (segment.getCurrentCount()>segment.capacity){
-
-                diff = segment.getCurrentCount()-segment.capacity
+        var y = 0
+        for (segment in segments) {
+            if (segment.getCurrentCount() > segment.capacity) {
+                diff = segment.getCurrentCount() - segment.capacity
                 x = diff
-                while (x > 0 ){
+
+                // calculate probabilities
+                var count1: Int = 0
+                var count2: Int = 0
+                var count3: Int = 0
+
+                for(mappedTrain in segmentTrainMapping[y]) {
+                    when(mappedTrain.priority) {
+                        1 -> count1++
+                        2 -> count2++
+                        3 -> count3++
+                        else -> {
+                            println("error: unknown priority")
+                        }
+                    }
+                }
+
+                if (count1 > 0 && count2 > 0 && count3 > 0) {
+                    probPrio1 = (100 / (count1 + (0.5 * count2) + (0.25 * count3))).toInt()
+                    probPrio2 = probPrio1 / 2
+                    probPrio3 = probPrio2 / 2
+                }
+                else if (count1 > 0 && count2 > 0 && count3 == 0) {
+                    probPrio1 = (100 / (count1 + (0.5 * count2) + (0.25 * count3))).toInt()
+                    probPrio2 = probPrio1 / 2
+                    probPrio3 = 0
+                }
+                else if (count1 > 0 && count2 == 0 && count3 > 0) {
+                    probPrio1 = (100 / (count1 + (0.5 * count2) + (0.25 * count3))).toInt()
+                    probPrio2 = 0
+                    probPrio3 = probPrio1 / 2
+                }
+                else if (count1 > 0 && count2 == 0 && count3 == 0) {
+                    probPrio1 = 100
+                    probPrio2 = 0
+                    probPrio3 = 0
+                }
+                else if (count1 == 0 && count2 > 0 && count3 > 0) {
+                    probPrio1 = 0
+                    probPrio2 = (100 / (count1 + (0.5 * count2) + (0.25 * count3))).toInt()
+                    probPrio3 = probPrio2 / 2
+                }
+                else if (count1 == 0 && count2 > 0 && count3 == 0) {
+                    probPrio1 = 0
+                    probPrio2 = 100
+                    probPrio3 = 0
+                }
+                else if (count1 == 0 && count2 == 0 && count3 > 0) {
+                    probPrio1 = 0
+                    probPrio2 = 0
+                    probPrio3 = 100
+                }
+
+                var delayList: MutableList<Int> = mutableListOf()
+
+                for(mappedTrain in segmentTrainMapping[y]) {
+                    var counter = 0
+                    when(mappedTrain.priority) {
+                        1 -> {
+                            counter = probPrio1
+                        }
+                        2 -> {
+                            counter = probPrio2
+                        }
+                        3 -> {
+                            counter = probPrio3
+                        }
+                        else -> {
+                            println("error: unknown priority")
+                        }
+                    }
+                    while (counter > 0) {
+                        delayList.add(mappedTrain.id)
+                        counter--
+                    }
+                }
+
+                while (x > 0) {
                     var differentTrain: Boolean = false
                     while (!differentTrain) {
-                        val newValue = Random().nextInt(segment.getCurrentCount())
-                        if(newValue !in delayedTrains) {
-                            delayedTrains.add(newValue)
+                        val newValue = Random().nextInt(delayList.size)
+                        if(delayList[newValue] !in delayedTrains) {
+                            delayedTrains.add(delayList[newValue])
                             differentTrain = true
                         }
                     }
                     x--
-
                 }
-                y = 0
 
-                for (train in trains){
-                    if (train.getCurrentSegment(currentStep)==segment.id){
-                        for (delayedTrain in delayedTrains){
-                            if (delayedTrain == y){
-                                train.setDelayed(true)
-
-                            }
-                        }
-
-                        y++
+                for(mappedTrain in segmentTrainMapping[y]) {
+                    if(mappedTrain.id in delayedTrains) {
+                        mappedTrain.setDelayed(true)
+                        mappedTrain.shiftSchedule(currentStep)
                     }
-
                 }
             }
             segment.persistCapacity()
+
+            y++
         }
+
+        for (train in trains) {
+            train.persistStep(currentStep)
+        }
+
         currentStep++
 
 
